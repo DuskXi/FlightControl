524d4cb1d95302817f0cfc4021c210d31b1180c9
DuskXi <DuskXi>
2022-01-20 20:38:40 +0000

    控制端/无人机 泛用化

--- RadioCommunication/Connector.py
+++ RadioCommunication/Connector.py
@@ -17,12 +17,13 @@ def readFile(fileName, encoding='utf-8'):
 
 
 class RadioConnector:
-    def __init__(self, configPath=None):
+    def __init__(self, configType, configPath=None):
         if configPath is None:
             configPath = os.path.join(re.sub(r'[^\\/]+$', '', os.path.realpath(__file__)), "config.json")
-
+        if configType not in ["drone", "controller"]:
+            raise Exception("configType must be drone or controller")
         self.configPath = configPath
-        self.config = json.loads(readFile(configPath))["drone"]
+        self.config = json.loads(readFile(configPath))[configType]
         # read config
         self.channel = self.config['channel']
         self.baudRate = self.config['baudRate']
@@ -38,25 +39,34 @@ class RadioConnector:
         self.dataPacket = DataPacket()
         self.connectionStatus = ConnectionStatus.Unknown
 
+        self.enableMessageLog = False
+
     def _openSerial(self, deviceName, baudRate):
         self.serial = serial.Serial(deviceName, baudRate, timeout=self.timeout)
+        logger.info(f"Connected to serial {deviceName} on {baudRate}")
         self.connectionStatus = ConnectionStatus.Connected
 
     def _closeSerial(self):
         if self.serial is not None:
             self.connectionStatus = ConnectionStatus.Disconnected
             self.serial.close()
+            logger.info(f"Serial {self.serialName} closed")
 
     def setRadio(self):
         outputBuffer = ""
         start = None
         self._openSerial(self.serialName, self.settingBaudRate)
+        logger.info("Start set radio")
+        logger.debug("Waiting for setting mode...")
         while True:
             outputBuffer += self.serial.read_all().decode('utf-8')
             if re.match(r'^#1 UartConfig', outputBuffer):
+                logger.debug(f"Got UartConfig:")
+                time.sleep(0.5)
                 outputBuffer = ""
-                command = self.templateSettingCommand.format(baudRate=self.baudRate, channel=self.channel)
+                command = self.templateSettingCommand.format(baudRate=self.dictBaudRate[str(self.baudRate)], channel=self.channel)
                 self.serial.write(command.encode('utf-8'))
+                logger.debug(f"Set radio: {command}")
                 start = time.time()
             if start is not None:
                 if time.time() - start > self.timeout:
@@ -78,6 +88,11 @@ class RadioConnector:
         while data == b'':
             data = self.serial.read_all()
             outputBuffer += data
+            if self.enableMessageLog and data != b'':
+                logger.debug(f"Received radio message({len(data)}): {data}")
+
+        if self.enableMessageLog:
+            logger.debug(f"Get radio message({len(outputBuffer)}): {outputBuffer}")
         return outputBuffer
 
     def threadReceive(self):
@@ -87,11 +102,19 @@ class RadioConnector:
             if type(decoded) != bool:
                 for decodedData in decoded:
                     self.recvBuffer.append({"data": decodedData, "time": time.time()})
+                    if self.enableMessageLog:
+                        logger.debug(f"Message Log:")
+                        logger.debug(f"    Received message(Bytes): {decodedData}")
+                        logger.debug(f"    Received message(Decoded): {decodedData.decode('utf-8')}")
 
     def startRadioCommunication(self):
         self._openSerial(self.serialName, self.baudRate)
         self.recvThread = threading.Thread(target=self.threadReceive)
         self.recvThread.start()
+
+    def stopRadioCommunication(self):
+        self._closeSerial()
+        self.recvThread.join()
 
 
 class ConnectionStatus(Enum):
